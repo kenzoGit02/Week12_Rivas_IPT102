c8fdcf4e50fa3191e5c59f7f95be532586f1c64b
Kenzo <kenzorivas11@gmail.com>
2022-11-06 13:43:22 +0800

    Add project files.

---
+++ Week12_Rivas_IPT102/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week12_Rivas_IPT102.MvcApplication" Language="C#" %>
---
+++ Week12_Rivas_IPT102/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week12_Rivas_IPT102.Models
+{
+    public class Student
+    {
+        [Required]
+        public int StudentID { get; set; }
+
+        [Required]
+        public string StudentName { get; set; }
+
+        [Range(18,40)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter Email ID")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Mobile No")]
+        [Display(Name = "Contact Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(11, ErrorMessage = "Number not be Exceeded")]
+        public int Mobile { get; set; }
+    }
+}
